2200275f7a6c589a37be9e23603f2dc41501f5f6
myhau <mmyhau@gmail.com>
2015-10-08 15:33:24 +0200

    gulp move also css styles

--- gulpfile.js
+++ gulpfile.js
@@ -147,7 +147,7 @@ gulp.task('buildBundle', ['styles', 'buildScripts', 'bower'], function() {
 
 // Bower helper
 gulp.task('bower', function() {
-  gulp.src('app/bower_components/**/*.js', {
+  gulp.src('app/bower_components/**/*.{js,css}', {
       base: 'app/bower_components'
     })
     .pipe(gulp.dest('dist/bower_components/'));
